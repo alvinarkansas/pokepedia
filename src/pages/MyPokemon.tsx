ef08f3d5ebcb7cdfd2e30bdedd64ce18d059424a
alvinarkansas <alvinarkansas@gmail.com>
2022-01-30 16:46:59 +0700

    added more media queries to my pokemon grid

--- src/pages/MyPokemon.tsx
+++ src/pages/MyPokemon.tsx
@@ -25,20 +25,19 @@ const Header = styled("header")({
   margin: "16px 0",
 });
 
-const Grid = styled("div")(
-  {
-    display: "grid",
-    gap: "16px",
+const Grid = styled("div")({
+  display: "grid",
+  gap: "16px",
+  "@media (min-width: 640px)": {
+    gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
   },
-  `
-  @media (min-width: 640px) {
-    grid-template-columns: repeat(2, minmax(0, 1fr));
-  }
-  @media (min-width: 1024px) {
-    grid-template-columns: repeat(4, minmax(0, 1fr));
-  }
-  `
-);
+  "@media (min-width: 1024px)": {
+    gridTemplateColumns: "repeat(3, minmax(0, 1fr))",
+  },
+  "@media (min-width: 1280px)": {
+    gridTemplateColumns: "repeat(4, minmax(0, 1fr))",
+  },
+});
 
 const EmptyState = styled("div")({
   height: "50vh",
